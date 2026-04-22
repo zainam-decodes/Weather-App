aacd1dcdb40da4a702298937dc1b39dae91450bd
Zainab Jahan Umaima <thezainabjahan14@gmail.com>
2026-04-22 23:39:55 +0530

    Delete front directory

--- front/config.js
+++
@@ -1,2 +0,0 @@
-// config.js
-const API_KEY = "c32f431ba40c950e5ad94efa2868c173";
--- front/radar.js
+++
@@ -1,9 +0,0 @@
-// radar.js
-function loadRadarMap(city) {
-  const radarImage = `
-    <img src="https://tile.openweathermap.org/map/precipitation_new/10/524/391.png?appid=${API_KEY}" 
-         alt="Radar image" style="width:100%; max-width:600px;">
-    <p style="font-size: 0.8rem;">📍 Sample radar map using OpenWeatherMap tiles</p>
-  `;
-  document.getElementById("radarMap").innerHTML = radarImage;
-}
